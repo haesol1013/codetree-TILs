28a40240d8cea5dc03cff2df802398d3fe54d718
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 22:26:39 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/정수 복사/copy-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    int b = 4;
+    b = a;
+
+    cout << a << " " << b << endl;
+    cout << a * b;
+    
+    return 0;
+}
