6fd0541bae93a1c032a2d13b16795590d311ba83
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 19:05:48 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+
+    int a = 13;
+    double b = 0.165000;
+
+    
+    cout << fixed;
+    cout.precision(6);
+    cout << a << " * " << b << " = " << a * b;
+
+    return 0;
+}
