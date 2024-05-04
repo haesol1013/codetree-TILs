7097afc38a74edb41ca11d6d35f203c1d0c198b8
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 18:53:09 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 7;
+    int b = 23;
+    int c = 30;
+    cout << a << " + " << b << " = " << c;
+    return 0;
+}
