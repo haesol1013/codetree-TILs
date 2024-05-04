d7006f4832960203953db84ec7c4657888ed1c41
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 15:55:43 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Total days in Year\n";
+    cout << 365 << endl;
+    cout << "Circumference rate\n";
+    cout << "3.1415926535";
+    return 0;
+}
