1600a714c58240acdf3f8a6b0ae081cca3b04084
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 18:58:23 +0900

    Title: 변수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    string b = "C";
+    cout << a << "..." << b;
+    return 0;
+}
