cd575ef018e3376a8f2737df4b948b7ed5d55927
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 15:53:38 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Hello students!";
+    cout << endl;
+    cout << "Welcome to LeebrosCode!";
+    return 0;
+}
