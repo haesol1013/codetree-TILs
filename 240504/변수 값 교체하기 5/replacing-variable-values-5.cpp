344613594b18715403bed207c5ac57d25a6a0cad
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 22:25:26 +0900

    Title: 변수 값 교체하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 2;
+    int b = 6;
+    a = b;
+
+    cout << a << endl << b;
+    
+    return 0;
+}
