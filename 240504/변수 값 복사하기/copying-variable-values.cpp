ea3d0150bf0d705babe8c5b021bc32fb75988c8a
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 22:38:06 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1, b = 2, c = 3;
+    a = b = c;
+
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
