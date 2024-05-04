d5eccb567d6765be3a0d00b3b3e3b0b31ffbd033
Shin Haesol <shinhaesol11@gmail.com>
2024-05-04 15:59:30 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 97;
+    int b = 13;
+    cout << a << " - " << b << " = " << a - b;
+    return 0;
+}
