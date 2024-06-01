5f2deb49fdd01953924d13cf725e0fe55d2bdad9
Shin Haesol <shinhaesol11@gmail.com>
2024-06-01 13:03:17 +0900

    Title: 가장 긴 증가하는 부분 수열 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240601/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.py
+++ 240601/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.py
@@ -4,9 +4,8 @@ input = sys.stdin.readline
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
-
 idx = 0
-while len(arr)>1:
+while len(arr)>1 and idx < len(arr):
     if arr[idx] > arr[idx+1]:
         arr.remove(arr[idx+1])
         idx = 0
