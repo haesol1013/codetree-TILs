a466f82696173d40702ea83a0850b712a752b3bf
Shin Haesol <shinhaesol11@gmail.com>
2024-07-17 20:28:48 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240717/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,29 @@
+# 입력
+n, m, k = map(int, input().split())
+matrix = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+k -= 1
+start, end = k, k+m
+curr_row = 0
+
+
+# 다음 행 확인
+def is_empty(arr):
+    return sum(arr) == 0
+
+
+while True:
+    if is_empty(matrix[curr_row+1][start:end]):
+        curr_row += 1
+    else:
+        break
+
+# 채우기
+for i in range(start, end):
+    matrix[curr_row][i] = 1
+
+# 출력
+for row in matrix:
+    print(*row)
