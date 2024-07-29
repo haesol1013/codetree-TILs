369ced3dce61d63c4c3d9951b275f705cc32dbe7
Shin Haesol <shinhaesol11@gmail.com>
2024-07-29 19:37:49 +0900

    Title: 피보나치 수, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/피보나치 수/fibonacci-number.py
@@ -0,0 +1,14 @@
+n = int(input())
+fibo_li = [0, 1, 1] + [-1]*(n-2)
+
+
+def fibo(n):
+    if fibo_li[n-1] != -1:
+        fibo_li[n] = fibo_li[n-1] + fibo_li[n-2]
+    else:
+        fibo_li[n] = fibo(n-1) + fibo(n-2)
+
+    return fibo_li[n]
+
+
+print(fibo(n))
